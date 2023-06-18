bb36e3072fc7db064556e82ca2a4a36d5a4d0386
samliweisen <weisen.li@hotmail.com>
2023-06-18 13:38:35 +0000

    feat(screen): add todo list screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import './screens/add_todo_page.dart';
+import './screens/todo_list_page.dart';
 import './providers/todo_provider.dart';
-import './widgets/todo_list.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,48 +21,6 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.green,
         ),
         home: TodoScreen(),
-      ),
-    );
-  }
-}
-
-class TodoScreen extends StatelessWidget {
-  final TextEditingController _textEditingController = TextEditingController();
-
-  TodoScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final todoProvider = Provider.of<TodoProvider>(context);
-
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Todo App'),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-              child: TodoList(
-            tasks: todoProvider.tasks,
-            onPressed: todoProvider.removeTaskAtIndex,
-          )),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: ElevatedButton(
-              onPressed: () {
-                todoProvider.fetchTasks();
-              },
-              child: const Text('Fetch Tasks'),
-            ),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => {
-          Navigator.push(
-              context, MaterialPageRoute(builder: (context) => AddTodoScreen()))
-        },
-        child: const Icon(Icons.add),
       ),
     );
   }
---
+++ lib/screens/todo_list_page.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import './add_todo_page.dart';
+import '../widgets/todo_list.dart';
+import '../providers/todo_provider.dart';
+
+class TodoScreen extends StatelessWidget {
+  final TextEditingController _textEditingController = TextEditingController();
+
+  TodoScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final todoProvider = Provider.of<TodoProvider>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Todo App'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+              child: TodoList(
+            tasks: todoProvider.tasks,
+            onPressed: todoProvider.removeTaskAtIndex,
+          )),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: ElevatedButton(
+              onPressed: () {
+                todoProvider.fetchTasks();
+              },
+              child: const Text('Fetch Tasks'),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => AddTodoScreen()))
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
