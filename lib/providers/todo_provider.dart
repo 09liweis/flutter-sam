ec99ce649cb1735a2bb0be77fa59bfd7900da641
samliweisen <weisen.li@hotmail.com>
2024-01-11 15:09:58 +0000

    update fetchMovies with adding parameter

--- lib/providers/todo_provider.dart
+++ lib/providers/todo_provider.dart
@@ -35,11 +35,11 @@ class MainProvider extends ChangeNotifier {
 
   List<Movie> movies = [];
 
-  Future<void> fetchMovies() async {
+  Future<void> fetchMovies(endPoint) async {
     loading = true;
     notifyListeners();
     try {
-      movies = await MovieService.fetchMovies();
+      movies = await MovieService.fetchMovies(endPoint);
     } catch (error) {
       throw Exception('Failed to fetch movies: $error');
     }
