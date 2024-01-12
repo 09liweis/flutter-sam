a9129f06befe04f2f3680a17ab620a3c101cfabc
samliweisen <weisen.li@hotmail.com>
2024-01-12 20:20:55 +0000

    refactor todoitem

--- lib/widgets/todo_list.dart
+++ lib/widgets/todo_list.dart
@@ -2,6 +2,40 @@ import 'package:flutter/material.dart';
 import 'package:fluttersam/models/todo.dart';
 import '../screens/add_todo_page.dart';
 
+class TodoItem extends StatelessWidget {
+  final Todo task;
+  const TodoItem({super.key, required this.task});
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: () => {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => AddTodoScreen()))
+      },
+      leading: const Icon(Icons.list),
+      title: Text(
+        task.getName(),
+        style: TextStyle(
+            decoration: (task.isDone())
+                ? TextDecoration.lineThrough
+                : TextDecoration.none),
+      ),
+      subtitle: Text(
+        task.getDate(),
+        style: const TextStyle(color: Colors.cyan, fontSize: 15),
+      ),
+      trailing: IconButton(
+        icon: const Icon(
+          Icons.delete,
+          color: Colors.red,
+          size: 30,
+        ),
+        onPressed: () {},
+      ),
+    );
+  }
+}
+
 class TodoList extends StatelessWidget {
   final List<Todo> tasks;
   final Function(int) onPressed;
@@ -14,34 +48,7 @@ class TodoList extends StatelessWidget {
       itemCount: tasks.length,
       itemBuilder: (context, index) {
         final task = tasks[index];
-        return ListTile(
-          onTap: () => {
-            Navigator.push(context,
-                MaterialPageRoute(builder: (context) => AddTodoScreen()))
-          },
-          leading: const Icon(Icons.list),
-          title: Text(
-            task.getName(),
-            style: TextStyle(
-                decoration: (task.isDone())
-                    ? TextDecoration.lineThrough
-                    : TextDecoration.none),
-          ),
-          subtitle: Text(
-            task.getDate(),
-            style: const TextStyle(color: Colors.cyan, fontSize: 15),
-          ),
-          trailing: IconButton(
-            icon: const Icon(
-              Icons.delete,
-              color: Colors.red,
-              size: 30,
-            ),
-            onPressed: () {
-              onPressed(index);
-            },
-          ),
-        );
+        return TodoItem(task: task);
       },
     );
   }
