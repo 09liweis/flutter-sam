a1d4f7072b40fdef1714b297bfc25cb4ff8e5684
samliweisen <weisen.li@hotmail.com>
2024-01-21 14:11:16 +0000

    call provider fetchMovieSummary service in movie detail screen

--- lib/screens/movies/movie_detail_screen.dart
+++ lib/screens/movies/movie_detail_screen.dart
@@ -1,14 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:fluttersam/models/movie.dart';
+import 'package:fluttersam/providers/app_provider.dart';
+import 'package:provider/provider.dart';
 
-class MovieDetailScreen extends StatelessWidget {
-  final Movie movie;
-  const MovieDetailScreen({super.key, required this.movie});
+class MovieDetailScreen extends StatefulWidget {
+  const MovieDetailScreen({super.key});
+  @override
+  _MovieDetailState createState() => _MovieDetailState();
+}
+
+class _MovieDetailState extends State<MovieDetailScreen> {
+  Movie movie = Movie();
+  @override
+  void initState() {
+    super.initState();
+    Provider.of<MainProvider>(context, listen: false).fetchMovieSummary();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(movie.getTitle())),
-      body: Text("Movie Detail Screen"),
+      body: Center(
+        child: Text(movie.getSummaryAPI()),
+      ),
     );
   }
 }
