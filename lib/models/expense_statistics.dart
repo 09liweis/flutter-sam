e9c1d7ab5c0bd551b3aad8a83bd6096e33506797
samliweisen <weisen.li@hotmail.com>
2024-01-27 13:55:18 +0000

    add expense statistics mdel

--- lib/models/expense_statistics.dart
+++ lib/models/expense_statistics.dart
@@ -7,7 +7,7 @@ class ExpenseStatistics {
 
   double total;
   String date;
-  Map<String, dynamic>? categoryPrice;
+  List<CategoryExpenses>? categoryPrice;
 
   double getTotal() {
     return total;
@@ -17,14 +17,45 @@ class ExpenseStatistics {
     return date;
   }
 
-  Map<String, dynamic>? getCategoryPrice() {
+  List<CategoryExpenses>? getCategoryPrice() {
     return categoryPrice;
   }
 
-  factory ExpenseStatistics.fromMap(Map<String, dynamic> map) =>
+  factory ExpenseStatistics.fromMap(Map<String, dynamic> json) =>
       ExpenseStatistics(
-        total: map['total'],
-        date: map['date'],
-        categoryPrice: map['categoryPrice'],
+        total: json['total'],
+        date: json['date'],
+        categoryPrice: List<CategoryExpenses>.from(
+            json['categoryPrice'].map((c) => CategoryExpenses.fromJson(c))),
       );
+}
+
+class CategoryExpenses {
+  double total;
+  String category;
+  List<Expense>? items;
+  CategoryExpenses({this.total = 0, this.category = '', this.items});
+  factory CategoryExpenses.fromJson(Map<String, dynamic> json) {
+    return CategoryExpenses(
+        total: json['total'],
+        category: json['category'],
+        items: List<Expense>.from(
+            json['items'].map((item) => Expense.fromJson(item))));
+  }
+}
+
+class Expense {
+  String title;
+  double price;
+  String category;
+  String date;
+  Expense(
+      {this.title = '', this.price = 0, this.category = '', this.date = ''});
+  factory Expense.fromJson(Map<String, dynamic> json) {
+    return Expense(
+        price: json['price'],
+        title: json['title'],
+        category: json['category'],
+        date: json['date']);
+  }
 }
