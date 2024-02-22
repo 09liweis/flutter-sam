df97de477180ca90067de9b559b4923168258f7d
samliweisen <weisen.li@hotmail.com>
2024-02-22 19:42:04 +0000

    fix networkUrl

--- lib/widgets/video.dart
+++ lib/widgets/video.dart
@@ -18,7 +18,7 @@ class _TikTokVideoState extends State<TikTokVideo> {
   @override
   void initState() {
     super.initState();
-    _controller = VideoPlayerController.network(widget.videoUrl)
+    _controller = VideoPlayerController.networkUrl(Uri.parse(widget.videoUrl))
       ..initialize().then((_) {
         setState(() {
           _isPlaying = true;
